b8a2033d0ca2e2440ec4aa8770e028bba2e0372a
Ren Chunhui <chunhui@RendeMBP.lan>
2018-01-04 10:20:56 +0800

    change test
    Former-commit-id: 8b1a20f04f2558f059d131e25162361eb9bc1245
    Former-commit-id: 28da913913937ba604099d845fa28f2c62c9f4ce

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-name="mars"
-echo $name
+. scripts/function.sh
+
+print_info 'Prompting for sudo password...'
+if sudo -v; then
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    print_success "Sudo credentials updated."
+else
+    print_error "Failed to obtain sudo credentials."
+fi
