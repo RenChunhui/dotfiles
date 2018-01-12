047704238121e0d385e686fcee40485b68327bfc
Ren Chunhui <chunhui@RendeMBP.lan>
2018-01-12 12:27:45 +0800

    commit
    Former-commit-id: d0311ba808c806e0602e3bff3091d3c143d03bc1
    Former-commit-id: a489fb6f47ed7ee14949130f82188aa81339c804

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # Prompting for sudo password
 . scripts/function.sh
 
+# 判断是否安装了 Homebrew
+if test ! $(which brew)
+then
+	print_info '安装 Homebrew...'
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 print_info 'Prompting for sudo password...'
 if sudo -v; then
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,4 +2,6 @@ if [[ ! -d $dir/oh-my-zsh ]]; then
 	print_info 'Installing Oh My Zsh...'
 
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
