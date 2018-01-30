7d63fcd04a50d50d34a4a3175437996251cea371
Ren Chunhui <chunhui@RendeMBP.lan>
2018-01-30 16:40:16 +0800

    neovim config
    Former-commit-id: 55e8c6dd4b56fc4d5ddbc5062b9400bd6ec41713
    Former-commit-id: 1da959af226444c1e08bda23f29c6a6550598cca

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ source scripts/print.sh
 # packages install
 source packages/setup.sh
 
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/.vim $HOME/.vim
+# symlink
+if (!isdirectory(expand("$HOME/.config/nvim")))
+  call system(expand("mkdir -p $Home/.config/nvim"))
+endif
+ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
