4d90046029c13fcbc0fe40073363ef624f6a635e
Chunhui Ren <renchunhui2008@gmail.com>
2020-09-23 16:08:55 +0800

    refresh

--- commands/brew.sh
+++ commands/brew.sh
@@ -6,10 +6,7 @@ source $BIN_PATH/lib/console
 source $BIN_PATH/lib/inquirer
 
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || exit 1
-  if [[ $? == 1 ]]; then
-    error "Please change mirro" && exit 0
-  fi
+  /bin/bash $BIN_PATH/lib/brew_install
 else
   ok "Hombrew has already installed."
 fi
