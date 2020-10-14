3aed55abf24e1dbda270193bc08a4136aa66b2ed
renchunhui <renchunhui2008@gmail.com>
2020-10-14 23:14:20 +0800

    add uninstall

--- workflow/brew/main.sh
+++ workflow/brew/main.sh
@@ -3,8 +3,7 @@
 source $BIN_PATH/lib/console
 
 if test ! $(which brew); then
-  /bin/bash $BIN_PATH/workflow/brew/install.sh
-  HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git /bin/bash $BIN_PATH/workflow/install.sh
+  HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git /bin/bash $BIN_PATH/workflow/brew/install.sh
   git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
   git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
   git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
