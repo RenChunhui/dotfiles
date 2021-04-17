a7606ad85f24aca0b524b2015e53a370a30df2fc
renchunhui <renchunhui2008@gmail.com>
2021-04-17 19:33:44 +0800

    change

--- packages/code/main.sh
+++ packages/code/main.sh
@@ -6,12 +6,12 @@ source $BIN_PATH/lib/console
 
 main() {
   step "安装 VSCode 环境"
-  
+
   if test ! $(which code); then
     brew install visual-studio-code --cask
   fi
 
-  cat $BIN_PATH/packages/code/code.txt | xargs -n 1 code --install-extension --verbose
+  cat $BIN_PATH/packages/code/code.txt | xargs -n 1 code --install-extension
 
   rm -rf $HOME/Library/Application\ Support/Code/User/settings.json
   sudo ln -s $HOME/.config/dotfiles/packages/code/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
