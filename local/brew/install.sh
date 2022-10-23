9138b60c136dd5d8286a49a64cf05c4e686d292b
Chunhui Ren <chunhui@ChunhuideMacBook-Pro.local>
2022-10-23 12:05:58 +0800

    add Brewfile

--- local/brew/install.sh
+++ local/brew/install.sh
@@ -7,16 +7,8 @@ set -e
 CYAN=$'\e[36m'
 RESET=$'\e[0m'
 
-cat <<EOF
-${CYAN}
-┌───────────────────────────────── Homebrew ──────────────────────────────────┐
-│                        The package manager for macOS                        │
-└─────────────────────────────────────────────────────────────────────────────┘
-${RESET}
-EOF
-
 if ! command -v brew >/dev/null 2>&1; then
-  echo "  Installing Homebrew for you."
+  echo "Installing Homebrew for you."
 
   export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
   export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
